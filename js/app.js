e10f40c81bde6e8617e757c905a4b5041283d50e
Wojtek Opulski <wojtekopulski@gmail.com>
2023-05-05 17:47:15 +0200

    feat: simplify code

--- js/app.js
+++ js/app.js
@@ -1,150 +1,98 @@
 const App = {
-  $: {
-    menu: document.querySelector('[data-id="menu"]'),
-    menuItems: document.querySelector('[data-id="menu-items"]'),
-    resetBtn: document.querySelector('[data-id="reset-btn"]'),
-    newRonudBtn: document.querySelector('[data-id="new-round-btn"]'),
-    squares: document.querySelectorAll('[data-id="square"]'),
-  },
+	$: {
+		menu: document.querySelector('[data-id="menu"]'),
+		menuItems: document.querySelector('[data-id="menu-items"]'),
+		resetBtn: document.querySelector('[data-id="reset-btn"]'),
+		newRonudBtn: document.querySelector('[data-id="new-round-btn"]'),
+		squares: document.querySelectorAll('[data-id="square"]'),
+	},
 
-  state: {
-    Players: [
-      { id: 1, clickedSquares: [] },
-      { id: 2, clickedSquares: [] },
-    ],
+	state: {
+		Players: [
+			{ id: 1, clickedSquares: [], name: "Player 1" },
+			{ id: 2, clickedSquares: [], name: "Player 2" },
+		],
 
-    currentPlayerID: 1,
-  },
+		currentPlayer: null,
+	},
 
-  getGameStatus(state) {
-    const winningPatterns = [
-      [1, 2, 3],
-      [4, 5, 6],
-      [7, 8, 9],
-      [1, 4, 7],
-      [2, 5, 8],
-      [3, 6, 9],
-      [1, 5, 9],
-      [3, 5, 7],
-    ];
+	setCurrentPlayer(player) {
+		this.state.currentPlayer = player;
+	},
 
-    const Player1Squares = state.Players[0].clickedSquares;
-    const Player2Squares = state.Players[1].clickedSquares;
+	getGameStatus() {
+		const winningPatterns = [
+			[1, 2, 3],
+			[4, 5, 6],
+			[7, 8, 9],
+			[1, 4, 7],
+			[2, 5, 8],
+			[3, 6, 9],
+			[1, 5, 9],
+			[3, 5, 7],
+		];
+		const currentPlayer = this.state.currentPlayer;
 
-    let winner = null;
+		for (let pattern of winningPatterns) {
+			const winDetected = pattern.every((square) =>
+				currentPlayer.clickedSquares.includes(square)
+			);
+			if (winDetected) {
+				alert(`wins : ${currentPlayer.name}`);
+				break;
+			}
+		}
+	},
 
-    winningPatterns.forEach((pattern) => {
-      if (state.currentPlayerID === 1) {
-        const Player1Wins = pattern.every((v) => Player1Squares.includes(v));
-        if (Player1Wins) {
-          winner = "player1";
-        }
-      } else {
-        const Player2Wins = pattern.every((v) => Player2Squares.includes(v));
-        if (Player2Wins) {
-          winner = "player2";
-        }
-      }
+	init() {
+		App.setCurrentPlayer(App.state.Players[0]);
+		App.registerEventListeners();
+	},
 
-      // if (Player1Wins) {
-      //   winner = "player1";
-      // }
+	registerEventListeners() {
+		App.$.menu.addEventListener("click", (event) => {
+			App.$.menuItems.classList.toggle("hidden");
+		});
 
-      // if (Player2Wins) {
-      //   winner = "player2";
-      // }
-    });
+		App.$.resetBtn.addEventListener("click", (event) => {
+			console.log("Reset the game");
+		});
 
-    console.log(Player1Squares);
-    return {
-      status: "in progress",
-      winner,
-    };
-  },
+		App.$.newRonudBtn.addEventListener("click", (event) => {
+			console.log("Start new round");
+		});
 
-  init() {
-    App.registerEventListeners();
-  },
+		App.$.squares.forEach((square) => {
+			square.addEventListener("click", (event) => {
+				const players = App.state.Players;
+				const currentPlayer = App.state.currentPlayer;
+				const allClickedSquares = [
+					...players[0].clickedSquares,
+					...players[1].clickedSquares,
+				];
 
-  registerEventListeners() {
-    App.$.menu.addEventListener("click", (event) => {
-      App.$.menuItems.classList.toggle("hidden");
-    });
+				if (allClickedSquares.includes(+square.id)) {
+					return;
+				}
 
-    App.$.resetBtn.addEventListener("click", (event) => {
-      console.log("Reset the game");
-    });
+				const icon = document.createElement("i");
+				const classNames = {
+					1: ["fa-x", "yellow"],
+					2: ["fa-o", "turquoise"],
+				};
 
-    App.$.newRonudBtn.addEventListener("click", (event) => {
-      console.log("Start new round");
-    });
+				icon.classList.add("fa-solid", ...classNames[currentPlayer.id]);
+				square.replaceChildren(icon);
 
-    App.$.squares.forEach((square) => {
-      square.addEventListener("click", (event) => {
-        const currentPlayerID = App.state.currentPlayerID;
-        const Players = App.state.Players;
-        const Player1 = Players[0];
-        const Player2 = Players[1];
+				currentPlayer.clickedSquares.push(+square.id);
 
-        console.log(currentPlayerID);
+				App.getGameStatus();
 
-        const currentPlayer = currentPlayerID === 1 ? Player1 : Player2;
-        // console.log(Player1.clickedSquares);
-        // console.log(Player2.clickedSquares);
-
-        const isSquareBusy = (squreID) => {
-          const isBusy =
-            App.state.Players[0].clickedSquares.find(
-              (square) => square === squreID
-            ) ||
-            App.state.Players[1].clickedSquares.find(
-              (square) => square === squreID
-            );
-
-          return isBusy !== undefined;
-        };
-
-        // console.log(isSquareBusy(+square.id));
-
-        if (isSquareBusy(+square.id)) {
-          return;
-        }
-
-
-        // console.log(
-        //   "this is the current player" + JSON.stringify(currentPlayer)
-        // );
-
-        const icon = document.createElement("i");
-        // const classNames = {
-        //   1: ["fa-x", "yellow"],
-        //   2: ["fa-o", "turquoise"],
-        // };
-
-        if (currentPlayer.id === 1) {
-          icon.classList.add("fa-solid", "fa-x", "yellow");
-        } else {
-          icon.classList.add("fa-solid", "fa-o", "turquoise");
-        }
-        // console.log({ currentPlayer, App, this: this });
-        // icon.classList.add("fa-solid", ...classNames[currentPlayer.id]);
-        square.replaceChildren(icon);
-
-        currentPlayer.clickedSquares.push(+square.id);
-
-        const gameStatus = App.getGameStatus();
-        console.log(gameStatus);
-
-        App.state.currentPlayerID = currentPlayer.id === 1 ? 2:1;
-        this.state.currentPlayer2 = "dupa";
-        console.log(currentPlayer);
-
-        // console.log(
-        //   "this is the updated current player ID: " + App.state.currentPlayerID
-        // );
-      });
-    });
-  },
+				App.state.currentPlayer =
+					currentPlayer === players[0] ? players[1] : players[0];
+			});
+		});
+	},
 };
 
 window.addEventListener("load", App.init);
