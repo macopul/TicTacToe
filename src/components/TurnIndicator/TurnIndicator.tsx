efd6bb38a226c2c4a94f8e8eb84623853de55c48
unknown <maciopul@gmail.com>
2023-06-22 22:03:18 +0200

    added animation on turn indicator

--- src/components/TurnIndicator/TurnIndicator.tsx
+++ src/components/TurnIndicator/TurnIndicator.tsx
@@ -1,15 +1,23 @@
-import { Player } from '../../../ts/type';
-import './TurnIndicator.css'
+import { Player } from "../../../ts/type";
+import "./TurnIndicator.css";
 
 type TurnIndicatorProps = {
-  currentPlayer: Player
-}
+  currentPlayer: Player;
+};
 
-export default function TurnIndicator ({currentPlayer}: TurnIndicatorProps) {
+export default function TurnIndicator({ currentPlayer }: TurnIndicatorProps) {
   return (
-    <div className="turn" data-id="turn">
-      <i className={[currentPlayer.iconClass, currentPlayer.colorClass, 'fa-solid'].join(' ')}></i>
-      <p className={currentPlayer.colorClass}>Player {currentPlayer.id}, you're up!</p>
+    <div  className="turn" data-id="turn">
+      <i key={Math.random()}
+        className={[
+          currentPlayer.iconClass,
+          currentPlayer.colorClass,
+          "fa-solid",
+        ].join(" ")}
+      ></i>
+      <p className={currentPlayer.colorClass}>
+        Player {currentPlayer.id}, you're up!
+      </p>
     </div>
   );
 }
